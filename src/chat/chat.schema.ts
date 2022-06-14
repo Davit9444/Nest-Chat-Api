fa415071e75735f4eb2a102c725f38bbcafa343c
Davit Harutyunyan <93554762+Davit9444@users.noreply.github.com>
2022-06-14 07:45:34 -0700

    Delete chat.schema.ts

--- src/chat/chat.schema.ts
+++
@@ -1,8 +0,0 @@
-import * as mongoose from "mongoose";
-
-export const PostShcema = new mongoose.Schema({
-    userID1: {type: String, required: true},
-    userID2: {type: String, required: true},
-    Messages: {type: String, required: true},
-    Date: {type: String, required: true}
-})
